51a09b7b5dae687cb2a4d8a809a528c4fd2245ce
Tucker Hoog <tuckerhoog@tutanota.com>
2021-04-06 15:42:15 -0400

    fix bugs

--- local-store.js
+++ local-store.js
@@ -67,28 +67,9 @@ export function totalCaught() {
 
 export function totalEncountered(pokemon) {
 
-    const pokedex = getPokedex();
-
     let total = 0;
 
     total += pokemon.encountered;
 
     return total;
-}
-
-export function capturesEncounters() {
-
-    let currentPoke = getPokedex();
-    let matched = findById(pokemonList[0].pokemon, currentPoke);
-
-    if (matched) {
-        capturesOne.textContent = matched.captured;
-        encountersOne.textContent = matched.encountered;
-    }
-
-    if (totalCaptured === 10) {
-
-        window.location = 'results.html';
-
-    }
 }
